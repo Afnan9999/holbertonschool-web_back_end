2de2448c4854d8aa1cf9c528d34d281352dfe8c1
Afnan9999 <afnan-911@outlook.sa>
2026-04-05 17:21:38 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,30 +1,37 @@
 import fs from 'fs';
+
 function readDatabase(path) {
-  return new Promise((resolve, reject) => { // eslint-disable-line consistent-return
+  return new Promise((resolve, reject) => {
     if (!path) {
-      return reject(new Error('Cannot load the database'));
+      reject(new Error('Cannot load the database'));
+      return;
     }
+
     fs.readFile(path, 'utf8', (error, data) => {
       if (error) {
-        return reject(new Error('Cannot load the database'));
+        reject(new Error('Cannot load the database'));
+        return;
       }
-      const studentline = data.split('\n');
-      const students = studentline.slice(1);
-      const validStudents = students.filter((line) => line.trim() !== '');
+
+      const lines = data.split('\n');
+      const students = lines.slice(1).filter((line) => line.trim() !== '');
       const studentsByField = {};
 
-      for (const studentline of validStudents) {
-        const parts = studentline.split(',');
+      for (const line of students) {
+        const parts = line.split(',');
         const firstname = parts[0];
         const field = parts[parts.length - 1];
 
         if (!studentsByField[field]) {
           studentsByField[field] = [];
         }
+
         studentsByField[field].push(firstname);
       }
-      return resolve(studentsByField);
+
+      resolve(studentsByField);
     });
   });
 }
-module.exports = readDatabase;
+
+export default readDatabase;
