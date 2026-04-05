ecedec675997639c5d33259312691983b9960314
Afnan9999 <afnan-911@outlook.sa>
2026-04-05 18:37:05 +0300

    Update server.js

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -4,10 +4,8 @@ import router from './routes/index';
 const app = express();
 const port = 1245;
 
-app.use(router);
+app.use('/', router);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+app.listen(port);
 
 export default app;
